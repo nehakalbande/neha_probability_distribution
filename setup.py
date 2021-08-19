c1f603fce350ec3ab7331a7371f42adfc6dd42da
Neha Kalbande <neha.leenaharsh@gmail.com>
2021-08-19 15:23:55 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='neha_probability_distribution',
+        version="1.0", 
+        description='Gaussian and Binomial distributions',
+        packages=['neha_probability_distribution'],
+        author='Neha Kalbande',
+        author_email='neha.leenaharsh@gmail.com', 
+    zip_safe=False)
